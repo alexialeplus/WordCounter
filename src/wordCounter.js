0db73fc0cb1521266b33c739a435d31e49c64dd6
Alexia Leplus <alexia.leplus@epitech.eu>
2018-11-06 16:59:22 +0100

    main - Comments

--- src/wordCounter.js
+++ src/wordCounter.js
@@ -1,6 +1,13 @@
+/**
+ * Takes a text as a parameter and returns
+ * the number of occurrences for each word
+ * of the text
+ */
 function wordCounter (text, minLength) {
+  // Check parameters
   if (typeof text === 'string' && Number.isInteger(minLength)) {
     text = text.toLowerCase()
+    // Turn the text into an array of words
     let allWords = text.split(/[-\s,'.;:!?]/)
 
     if (allWords.length > 0) {
@@ -11,7 +18,7 @@ function wordCounter (text, minLength) {
         if (word.length >= minLength && alreadyUsed.indexOf(word) === -1) {
           let regEx = new RegExp('\\b' + word + '\\b', 'g')
           let occ = text.split(regEx).length - 1
-          alreadyUsed.push(word)
+          alreadyUsed.push(word) // Avoid repetition of code for the same word
           result[word] = occ
         }
       })
