aaad1b79aa82a874f0ced3740bce2e9c5873f90e
Alexia Leplus <alexia.leplus@epitech.eu>
2018-11-06 16:23:43 +0100

    main - function WordCounter ok for french test

--- src/wordCounter.js
+++ src/wordCounter.js
@@ -1,8 +1,28 @@
-function wordCounter(text, minLength) {
+function wordCounter (text, minLength) {
+  if (typeof text === 'string' && Number.isInteger(minLength)) {
+    text = text.toLowerCase()
+    let allWords = text.split(/[-\s,'.;:!?]/)
 
-    //
+    if (allWords.length > 0) {
+      let alreadyUsed = []
+      let result = {}
 
-    return {};
+      allWords.forEach(function (word) {
+        if (word.length >= minLength && alreadyUsed.indexOf(word) === -1) {
+          let regEx = new RegExp('\\b' + word + '\\b', 'g')
+          let occ = text.split(regEx).length - 1
+          alreadyUsed.push(word)
+          result[word] = occ
+        }
+      })
+
+      return result
+    } else {
+      return false
+    }
+  } else {
+    return false
+  }
 }
 
-module.exports = wordCounter;
+module.exports = wordCounter
